b5d95203cac11286a90d3fe418c8782baf31d54b
José Martinho <almofadex@gmail.com>
2020-03-14 00:07:55 +0000

    upload songs to spotify

--- music-identifier.py
+++ music-identifier.py
@@ -3,57 +3,71 @@ import json
 import os, sys
 from acrcloud.recognizer import ACRCloudRecognizer
 import secrets
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 
-"""user_id = secrets.user_id
+token = ""
+client_id = secrets.client_id
 client_secret=secrets.client_secret
+username = "ruc107.9"
+playlist = "0eWYkGTWmlJtuLksLwMwBh"
 
 
-request_body = json.dumps({
-    "name": "testeeeeekkkkk",
-    "description": "tche bro ganda teste",
-    "public": True
-})
-query = "https://api.spotify.com/v1/users/{}/playlists".format(user_id)
-response=requests.post(
-    query,
-    data=request_body,
-    headers={
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {}".format()
+def refresh():
+    global token
+    token = spotipy.util.prompt_for_user_token("ruc107.9",
+        "playlist-modify-public",
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri="http://localhost/")
+    return
+
+def main():
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    music = ""
+    artist = ""
+    config = {
+        #Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
+        'host':'identify-eu-west-1.acrcloud.com',
+        'access_key':'c20267418e799f23a3f94b14baf3a790', 
+        'access_secret':'aiwwg7KPELXaWt9AwvfYv6IclZSCTndEltCftKjJ',
+        'timeout':10 # seconds
     }
-)"""
+    re = ACRCloudRecognizer(config)
+    while(1):
+        os.system('sudo ./ripper.sh')
+
+
+        json_data = json.loads(re.recognize_by_file("/tmp/aaa.mp3", 0))
+        print(json.dumps(json_data))
+        if(json_data["status"]["msg"] == "Success"):
+            current_music = json_data["metadata"]["music"][0]["title"]
+            current_artist = json_data["metadata"]["music"][0]["artists"][0]["name"]
+            if current_music != music or current_artist != artist:
+                music = current_music
+                artist = current_artist
+                external = json_data["metadata"]["music"][0]["external_metadata"]
+                if "spotify" in external:
+                    spotify_id = external["spotify"]["track"]["id"]
+                    print("SPOT ID ", spotify_id)
+                    if(token):
+                        try:
+                            r = sp.user_playlist_remove_all_occurrences_of_tracks(username,playlist,[spotify_id])
+                            print(r)
+                            r = sp.user_playlist_add_tracks(username, playlist, [spotify_id])
+                            print(r)
+                        except spotipy.client.SpotifyException:
+                            print("exception")
+                            refresh()
+                            sp = spotipy.Spotify(auth=token)
+                            sp.trace = False
+                            sp.user_playlist_remove_all_occurrences_of_tracks(username,playlist,[spotify_id])
+                            sp.user_playlist_add_tracks(username, playlist, [spotify_id])
+        os.remove("/tmp/aaa.mp3")
+        os.remove("/tmp/aaa.cue")
 
 if __name__ == '__main__':
-    if(len(sys.argv) != 2):
-        print("usage: sudo python muic-identifier.py <path>")
-    else:
-        music = ""
-        config = {
-            #Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
-            'host':'identify-eu-west-1.acrcloud.com',
-            'access_key':'c20267418e799f23a3f94b14baf3a790', 
-            'access_secret':'aiwwg7KPELXaWt9AwvfYv6IclZSCTndEltCftKjJ',
-            'timeout':10 # seconds
-        }
-        re = ACRCloudRecognizer(config)
-        while(1):
-            os.system('sudo ./ripper.sh')
-
-
-            json_data = json.loads(re.recognize_by_file("/tmp/aaa.mp3", 0))
-            print(json.dumps(json_data))
-            if(json_data["status"]["msg"] == "Success"):
-                current_music = json_data["metadata"]["music"][0]["title"]
-                if current_music != music:
-                    f = open(sys.argv[1], "a")
-                    music = current_music
-                    print("vou escrever crl")
-                    f.write(json_data["metadata"]["music"][0]["artists"][0]["name"])
-                    f.write (" - ")
-                    f.write(music)
-                    f.write ("\n")
-                    f.close()
-
-            os.remove("/tmp/aaa.mp3")
-            os.remove("/tmp/aaa.cue")
+    refresh()
+    main()
